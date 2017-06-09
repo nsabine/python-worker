b05f8d1f1b8f96975cfea582f623275cf77e8c76
Nick Sabine <nick@esabine.net>
2017-06-09 17:22:36 -0400

    add stdout flush so prints are not held in buffer

--- worker.py
+++ worker.py
@@ -3,8 +3,10 @@
 import time
 import rediswq
 import os
+import sys
 
 print("Staring worker")
+sys.stdout.flush()
 
 host="redis"
 password=os.environ['REDIS_PASSWORD']
@@ -15,13 +17,18 @@ password=os.environ['REDIS_PASSWORD']
 q = rediswq.RedisWQ(name="job", host=host, password=password)
 print("Worker with sessionID: " +  q.sessionID())
 print("Initial queue state: empty=" + str(q.empty()))
+sys.stdout.flush()
+
 while not q.empty():
   item = q.lease(lease_secs=10, block=True, timeout=2) 
   if item is not None:
     itemstr = item.decode("utf=8")
     print("Working on " + itemstr)
+    sys.stdout.flush()
     time.sleep(10) # Put your actual work here instead of sleep.
     q.complete(item)
   else:
     print("Waiting for work")
+    sys.stdout.flush()
 print("Queue empty, exiting")
+sys.stdout.flush()
