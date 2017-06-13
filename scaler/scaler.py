934319443409870d6b78ba163dfc61b5d5baefca
Nick sabine <nsabine@redhat.com>
2017-06-13 16:33:54 -0400

    added downward api

--- scaler/scaler.py
+++ scaler/scaler.py
@@ -1,11 +1,15 @@
-import requests                                                                                                                                                                                                                        
 import os                                                                                                                                                                                                                              
 import sys
+from pprint import pprint
+
+from __future__ import print_function
 
 print("Starting scaler")
 sys.stdout.flush()
 
                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                       
+namespace=os.environ['NAMESPACE']                                                                                                                                                                                             
 host=os.environ['KUBERNETES_SERVICE_HOST']                                                                                                                                                                                             
 port=os.environ['KUBERNETES_SERVICE_PORT']                                                                                                                                                                                             
 tokenfile = open('/var/run/secrets/kubernetes.io/serviceaccount/token','r')                                                                                                                                                            
@@ -20,11 +24,14 @@ sys.stdout.flush()
                                                                                                                                                                                                                                        
 headers = {'Authorization': 'Bearer ' + token}                                                                                                                                                                                         
 r = requests.get(url, headers=headers, verify=False)
-print(r.text)
+pprint(r.text)
 sys.stdout.flush()
 
 
-url = 'https://' + host + ':' + port + '/oapi/v1/namespaces/' + namespace + '/deploymentconfigs/' + deploymentconfig + '/scale'
+url = 'https://' + host + ':' + port + '/oapi/v1/namespaces/' + namespace + '/deploymentconfigs/python-scaler'
 headers = {'Authorization': 'Bearer ' + token}                                                                                                                                                                                         
-r = requests.get(url, headers=headers, verify=False)
+patch = 'spec: \n  replicas: 2'
+r = requests.patch(url, headers=headers, json=patch, verify=False)
+pprint(r.text)
+sys.stdout.flush()
 
