61e3f641eaba552e216cc7340fd559e2bf83e19e
Nick sabine <nsabine@redhat.com>
2017-06-13 14:29:59 -0400

    fix port

--- scaler/scaler.py
+++ scaler/scaler.py
@@ -7,11 +7,13 @@ sys.stdout.flush()
 
                                                                                                                                                                                                                                        
 host=os.environ['KUBERNETES_SERVICE_HOST']                                                                                                                                                                                             
+port=os.environ['KUBERNETES_SERVICE_PORT']                                                                                                                                                                                             
 tokenfile = open('/var/run/secrets/kubernetes.io/serviceaccount/token','r')                                                                                                                                                            
 token=tokenfile.read()                                                                                                                                                                                                                      
-url = 'https://' + host + ':8443/oapi/v1'                                                                                                                                                                                              
+url = 'https://' + host + ':' + port + '/oapi/v1'                                                                                                                                                                                              
 
 print("host: " + host)
+print("post: " + post)
 print("url: " + url)
 print("token: " + token)
 sys.stdout.flush()
