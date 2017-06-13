708fe1ee999e68fd116e83a7e357ef726749db4b
Nick sabine <nsabine@redhat.com>
2017-06-13 14:23:28 -0400

    print output from scaler

--- scaler/scaler.py
+++ scaler/scaler.py
@@ -1,13 +1,21 @@
 import requests                                                                                                                                                                                                                        
 import os                                                                                                                                                                                                                              
+import sys
+
+print("Starting scaler")
+sys.stdout.flush()
+
                                                                                                                                                                                                                                        
 host=os.environ['KUBERNETES_SERVICE_HOST']                                                                                                                                                                                             
-                                                                                                                                                                                                                                       
 tokenfile = open('/var/run/secrets/kubernetes.io/serviceaccount/token','r')                                                                                                                                                            
-                                                                                                                                                                                                                                       
 token=tokenfile.read()                                                                                                                                                                                                                      
-                                                                                                                                                                                                                                       
 url = 'https://' + host + ':8443/oapi/v1'                                                                                                                                                                                              
+
+print("host: " + host)
+print("url: " + url)
+print("token: " + token)
+sys.stdout.flush()
                                                                                                                                                                                                                                        
 headers = {'Authorization': 'Bearer ' + token}                                                                                                                                                                                         
-requests.get(url, headers=headers, verify=False)  
+print(requests.get(url, headers=headers, verify=False))
+sys.stdout.flush()
