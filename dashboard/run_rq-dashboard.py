e2a670645da8fc0e34ab58e91d6d166a16c5280c
Nick sabine <nsabine@redhat.com>
2017-06-21 12:41:46 -0400

    attempt to add default route

--- dashboard/run_rq-dashboard.py
+++ dashboard/run_rq-dashboard.py
@@ -8,10 +8,14 @@ import os
 
 app = Flask(__name__)
 app.config.from_object(rq_dashboard.default_settings)
-app.register_blueprint(rq_dashboard.blueprint, url_prefix="/")
+app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
 
 app.config['REDIS_HOST'] = os.environ['DATABASE_SERVICE_NAME']
 app.config['REDIS_PASSWORD'] = os.environ['REDIS_PASSWORD']
+
+@app.route("/")
+def root():
+    redirect("/rq")
 
 @app.route("/hello")
 def hello():
