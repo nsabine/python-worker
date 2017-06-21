d49e46ccd39abbb5faa4f9bd0171ecf87e3362b3
Nick sabine <nsabine@redhat.com>
2017-06-21 16:24:34 -0400

    attempting to get the flask config correct

--- dashboard/run_rq-dashboard.py
+++ dashboard/run_rq-dashboard.py
@@ -8,19 +8,18 @@ import os
 
 app = Flask(__name__)
 app.config.from_object(rq_dashboard.default_settings)
-#app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
-app.register_blueprint(rq_dashboard.blueprint)
+app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
 
 app.config['REDIS_HOST'] = os.environ['DATABASE_SERVICE_NAME']
 app.config['REDIS_PASSWORD'] = os.environ['REDIS_PASSWORD']
 
-#@app.route("/")
-#def root():
-#    redirect("/rq")
+@app.route("/")
+def root():
+    redirect("/rq")
 
-#@app.route("/hello")
-#def hello():
-#    return "Hello World!"
+@app.route("/hello")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9181)
