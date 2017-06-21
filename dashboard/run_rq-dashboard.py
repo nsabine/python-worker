d0c4ba62ba63396b713ca1234f2055b73e120e3d
Nick sabine <nsabine@redhat.com>
2017-06-21 09:06:52 -0400

    debug dashboard

--- dashboard/run_rq-dashboard.py
+++ dashboard/run_rq-dashboard.py
@@ -1,3 +1,21 @@
+#import os
+#os.execl('/opt/app-root/run_rq-dashboard.sh', 'rq-dashboard')
+
+from flask import Flask
+import rq_dashboard
 import os
 
-os.execl('/opt/app-root/run_rq-dashboard.sh', 'rq-dashboard')
+
+app = Flask(__name__)
+app.config.from_object(rq_dashboard.default_settings)
+app.register_blueprint(rq_dashboard.blueprint, url_prefix="/")
+
+app.config['REDIS_HOST'] = os.environ['DATABASE_SERVICE_NAME']
+app.config['REDIS_PASSWORD'] = os.environ['REDIS_PASSWORD']
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
