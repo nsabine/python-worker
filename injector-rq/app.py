58254fb7e3dac787829f7fa99103265231bebfe0
Nick sabine <nsabine@redhat.com>
2017-06-25 19:07:23 -0400

    redis-queue version of injector

---
+++ injector-rq/app.py
@@ -0,0 +1,29 @@
+import os
+import redis
+#from redis import StrictRedis
+from rq import Connection, Worker
+
+# use the kubernetes service environment variables to
+#  connect to the redis queue
+
+REDIS_HOST =  os.environ['REDIS_SERVICE_HOST']
+REDIS_PORT =  os.environ['REDIS_SERVICE_PORT']
+REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+
+#REDIS_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+QUEUES = ['default']
+
+#connection=StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+
+def do_work():
+  time.sleep(1) # Put your actual work here instead of sleep.
+
+def add_jobs():
+  #redis_connection = redis.from_url(REDIS_URL)
+  redis_connection = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+  q = Queue(connection=redis_connection)
+  for i in 0..100:
+  job = q.enqueue(do_work, result_ttl=60000)
+
+if __name__ == '__main__':
+  add_jobs()
